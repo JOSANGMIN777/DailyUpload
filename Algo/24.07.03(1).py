f017323767de73e9727a560104c9e625f1a6fc29
JOSANGMIN777 <nice6122@naver.com>
2024-07-03 00:16:20 +0900

    0703

---
+++ Algo/24.07.03(1).py
@@ -0,0 +1,15 @@
+# 2609 최대공약수와 최소공배수 브론즈1
+def gcd(a,b):
+    while b > 0:
+        a,b = b, a % b
+    return a
+
+def lcm(a,b):
+    ans = a*b
+    return ans
+
+A,B = map(int, input().split())
+print(gcd(A,B))
+print(lcm(A,B)//gcd(A,B))
+
+# 유클리드 호제법 잊지말자 
