625bb1b8744824a1cbabb394ad5fa5574f3398b2
JOSANGMIN777 <nice6122@naver.com>
2024-07-11 23:53:18 +0900

    0711

---
+++ Algo/24.07.11.py
@@ -0,0 +1,12 @@
+# 31403 A+B-C 브론즈4
+
+A = input()
+B = input()
+C = input()
+AB = A+B
+
+print(int(A)+int(B)-int(C))
+print(int(AB)-int(C))
+
+# 하루종일 엘리스 챌린지 잡고있다가 결국 챌린지 문제도 못풀고 백준도 못풀었다... ㅠㅠ 
+# 근데 엘리스 문제가 이상했어 오늘은
